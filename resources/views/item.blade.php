82fa487a2877250aadc928d210ddc6d0c5845a48
Micheal Mand <micheal@kmdwebdesigns.com>
2017-03-23 12:02:22 -0600

    Fix errors with Blade parsing
    For some reason, if `@endif` is directly next to a bare string it doesn't parse correctly. This is one way to fix it, another would be to wrap them in Blade "echo" tags: `{{ 'active' }}@endif`

--- resources/views/item.blade.php
+++ resources/views/item.blade.php
@@ -1,4 +1,4 @@
-<li class="@if($item->getItemClass()){{ $item->getItemClass() }}@endif @if($active)active@endif @if($item->hasItems())treeview@endif clearfix">
+<li class="@if($item->getItemClass()){{ $item->getItemClass() }}@endif @if($active)active @endif @if($item->hasItems())treeview @endif clearfix">
     <a href="{{ $item->getUrl() }}" class="@if(count($appends) > 0) hasAppend @endif" @if($item->getNewTab())target="_blank"@endif>
         <i class="{{ $item->getIcon() }}"></i>
         <span>{{ $item->getName() }}</span>
